00c8b33db732710e0fbfbb3529d344d2ff2fbbd7
Paige Bolduc <paigebolduc@gmail.com>
2016-11-07 22:01:56 -0500

    Add initial solution for space-age exercise.

---
+++ exercism_exercises/space-age/space_age.exs
@@ -0,0 +1,31 @@
+defmodule SpaceAge do
+  @type planet :: :mercury | :venus | :earth | :mars | :jupiter
+                | :saturn | :neptune | :uranus
+
+  @doc """
+  Return the number of years a person that has lived for 'seconds' seconds is
+  aged on 'planet'.
+  """
+  @spec age_on(planet, pos_integer) :: float
+  def age_on(planet, seconds) do
+    case planet do
+      :earth ->
+        seconds / 31557600
+      :mercury ->
+        seconds / 31557600 / 0.2408467
+      :venus ->
+        seconds / 31557600 / 0.61519726
+      :mars -> 
+        seconds / 31557600 / 1.8808158
+      :jupiter ->
+        seconds / 31557600 / 11.862615
+      :saturn ->
+        seconds / 31557600 / 29.447498
+      :uranus -> 
+        seconds / 31557600 / 84.016846
+      :neptune ->
+        seconds / 31557600 / 164.79132
+    end
+
+  end
+end
