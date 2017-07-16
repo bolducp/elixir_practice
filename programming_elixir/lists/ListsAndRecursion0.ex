389cef66d5cad05986e80cf5596f233125565024
Paige Bolduc <paige@smartlogic.io>
2017-07-16 14:47:36 -0400

    Add Sum no acc challenge description

--- programming_elixir/lists/ListsAndRecursion0.ex
+++ programming_elixir/lists/ListsAndRecursion0.ex
@@ -1,4 +1,6 @@
-
+# I defined our sum function to carry a partial total around as a second parameter. 
+# I did this so I could illustrate how to use accumulators to build values.
+# The sum function can also be written without an accumulator. Can you do it?
 
 defmodule MyList do
   def sum(list), do: _sum(list,0)
