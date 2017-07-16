2aa0f194ea8b954e35663a5a32664f67a5060762
Paige Bolduc <paige@smartlogic.io>
2017-07-16 15:09:52 -0400

    Initial find max val in list function

---
+++ programming_elixir/lists/ListsAndRecursion2.ex
@@ -0,0 +1,19 @@
+# Write a max(list) that returns the element with the maximum value in the list.
+# (This is slightly trickier than it sounds.)
+
+
+defmodule MyList do
+  def max_val([ head | tail ]), do: _max_val(tail, head)
+
+  def _max_val([], max), do: max
+
+  def _max_val([ head | tail ], max) when head > max do
+    _max_val(tail, head)
+  end
+  def _max_val([ head | tail ], max), do: _max_val(tail, max)
+end
+
+IO.puts MyList.max_val([100, 55, 909, 3])
+IO.puts MyList.max_val([1001, 55, 909, 3])
+IO.puts MyList.max_val([100, 55, 909, 3000])
+IO.puts MyList.max_val([100])
