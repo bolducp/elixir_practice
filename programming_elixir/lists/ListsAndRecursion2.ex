d7aae12d4536911a6974d217e18ca58006bd236a
Paige Bolduc <paige@smartlogic.io>
2017-07-16 15:15:57 -0400

    Add gaurd clause for empy list case

--- programming_elixir/lists/ListsAndRecursion2.ex
+++ programming_elixir/lists/ListsAndRecursion2.ex
@@ -3,13 +3,14 @@
 
 
 defmodule MyList do
+  def max_val([]), do: []
+
   def max_val([ head | tail ]), do: _max_val(tail, head)
 
   def _max_val([], max), do: max
 
-  def _max_val([ head | tail ], max) when head > max do
-    _max_val(tail, head)
-  end
+  def _max_val([ head | tail ], max) when head > max, do: _max_val(tail, head)
+
   def _max_val([ head | tail ], max), do: _max_val(tail, max)
 end
 
@@ -17,3 +18,4 @@ IO.puts MyList.max_val([100, 55, 909, 3])
 IO.puts MyList.max_val([1001, 55, 909, 3])
 IO.puts MyList.max_val([100, 55, 909, 3000])
 IO.puts MyList.max_val([100])
+IO.puts MyList.max_val([])
